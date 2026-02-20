032021bf668ee0e820d260482b90a02ecf453bd5
Ryan Yocum <rtyocum1@gmail.com>
2026-02-20 11:53:58 -0500

    removed tester script, works with commander now

--- test_socket.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-HOST = "10.114.214.229"  # ESP32 IP — change to match your setup
-PORT = 8080
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello from Python")
-    print(f"Sent to {HOST}:{PORT}")
