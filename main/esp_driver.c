7c6c6a31e405dfd30b96b6a2380edee1109f7f3a
Ryan Yocum <rtyocum1@gmail.com>
2026-03-05 13:40:08 -0500

    Test new CI

--- main/esp_driver.c
+++ main/esp_driver.c
@@ -29,26 +29,26 @@ void app_main(void) {
   i2c_bus_t bus;
   i2c_bus_config_t bus_config = {
       .port = I2C_NUM_0,
-      .sda_io_num = CONFIG_MOTORHAT_SDA_PIN,
-      .scl_io_num = CONFIG_MOTORHAT_SCL_PIN,
+      .sda_io_num = CONFIG_DRIVER_MOTORHAT_SDA_PIN,
+      .scl_io_num = CONFIG_DRIVER_MOTORHAT_SCL_PIN,
   };
   ESP_ERROR_CHECK(i2c_bus_init(&bus, &bus_config));
 
   i2c_bus_t adc_bus;
   i2c_bus_config_t adc_bus_config = {
       .port = I2C_NUM_1,
-      .sda_io_num = CONFIG_ADS1015_SDA_PIN,
-      .scl_io_num = CONFIG_ADS1015_SCL_PIN,
+      .sda_io_num = CONFIG_DRIVER_ADS1015_SDA_PIN,
+      .scl_io_num = CONFIG_DRIVER_ADS1015_SCL_PIN,
   };
   ESP_ERROR_CHECK(i2c_bus_init(&adc_bus, &adc_bus_config));
 
   ads1015_handle_t ads;
   ads1015_config_t ads_config = {
-      .i2c_addr = CONFIG_ADS1015_ADDRESS,
+      .i2c_addr = CONFIG_DRIVER_ADS1015_ADDRESS,
       .i2c_speed_hz = 400000,
-      .rdy_gpio = CONFIG_ADS1015_RDY_PIN,
+      .rdy_gpio = CONFIG_DRIVER_ADS1015_RDY_PIN,
       .bus_handle = adc_bus.handle,
-      .adc_data_rate = CONFIG_ADS1015_DATA_RATE,
+      .adc_data_rate = CONFIG_DRIVER_ADS1015_DATA_RATE,
   };
   ESP_ERROR_CHECK(ads1015_init(&ads, &ads_config));
 
@@ -57,7 +57,7 @@ void app_main(void) {
   motorhat_config_t motorhat_config = {
       .pca9685_config =
           {
-              .i2c_addr = CONFIG_MOTORHAT_ADDRESS,
+              .i2c_addr = CONFIG_DRIVER_MOTORHAT_ADDRESS,
               .i2c_speed_hz = 400000,
               .pwm_freq_hz = DEFAULT_FREQUENCY_HZ,
               .bus_handle = bus.handle,
@@ -67,8 +67,8 @@ void app_main(void) {
   ESP_ERROR_CHECK(motorhat_init(&motorhat, &motorhat_config));
 
   driver_wifi_config_t wifi_config = {
-      .ssid = CONFIG_WIFI_SSID,
-      .password = CONFIG_WIFI_PASSWORD,
+      .ssid = CONFIG_DRIVER_WIFI_SSID,
+      .password = CONFIG_DRIVER_WIFI_PASSWORD,
   };
 
   ESP_ERROR_CHECK(wifi_init(&wifi_config));
@@ -76,8 +76,8 @@ void app_main(void) {
 
   driver_socket_handle_t socket_handle;
   driver_socket_config_t socket_config = {
-      .ip = CONFIG_SERVER_IP,
-      .port = CONFIG_SERVER_PORT,
+      .ip = CONFIG_DRIVER_SERVER_IP,
+      .port = CONFIG_DRIVER_SERVER_PORT,
   };
 
   ESP_ERROR_CHECK(driver_socket_init(&socket_handle, &socket_config));
